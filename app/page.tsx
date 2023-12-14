d15d75e8d01dfc5c567a51daf59998565a8348b3
Brett Chalupa <brettchalupa@gmail.com>
2023-12-14 12:56:17 -0500

    add urql & register client
    from following https://formidable.com/open-source/urql/docs/advanced/server-side-rendering/#nextjs

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,18 @@ export const metadata = {
   title: 'App Router',
 }
 
+import { cacheExchange, createClient, fetchExchange, gql } from '@urql/core';
+import { registerUrql } from '@urql/next/rsc';
+
+const makeClient = () => {
+  return createClient({
+    url: 'https://trygql.formidable.dev/graphql/basic-pokedex',
+    exchanges: [cacheExchange, fetchExchange],
+  });
+};
+
+const { getClient } = registerUrql(makeClient);
+
 export default function Page() {
   return <h1>App Router</h1>
 }
